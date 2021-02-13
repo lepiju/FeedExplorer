c4543cf740dec04bf7220a6077392c689fb4e6e4
Julien Lépine <lepi.julien@gmail.com>
2021-02-13 14:12:11 -0500

    Show map inside view controller

--- FeedExplorer/Managers/NetworkManager.swift
+++ FeedExplorer/Managers/NetworkManager.swift
@@ -15,17 +15,6 @@ class NetworkManager {
     func fetchMap(completionHandler: @escaping (Map) -> Void) {
         let url = URL(string: feedUrl)!
         let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-            
-            if let error = error {
-                print("Error with fetching films: \(error)")
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                print("Error with the response, unexpected status code: \(String(describing: response))")
-                return
-            }
 
             if let data = data,
                let map = try? JSONDecoder().decode(Map.self, from: data) {
--- FeedExplorer/ViewController.swift
+++ FeedExplorer/ViewController.swift
@@ -6,19 +6,72 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Views
+    
+    private let mapView: MKMapView = {
+        let view = MKMapView()
+        view.mapType = MKMapType.standard
+        view.isZoomEnabled = true
+        view.isScrollEnabled = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let loadingView: UIView = {
+        let view = UIView()
+        view.isHidden = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // MARK: Properties
     
     private var map: Map?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViewHierarchy()
+        setupConstraints()
+        
         
         sharedNetworkManager.fetchMap(completionHandler: { [weak self] (map) in
             self?.map = map
             print(map.feeds)
         })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    private func setupViewHierarchy() {
+        view.addSubview(mapView)
+        view.addSubview(loadingView)
+    }
+    
+    private func setupConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += [
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ]
+        
+        constraints += [
+            loadingView.topAnchor.constraint(equalTo: view.topAnchor),
+            loadingView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            loadingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            loadingView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
 }
 
