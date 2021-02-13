fd4e2965544156998b79dd7f8199c218b663cdb9
Julien Lépine <lepi.julien@gmail.com>
2021-02-13 18:00:58 -0500

    Add CountryColor UIColor extension

---
+++ FeedExplorer/Style/CountryColor.swift
@@ -0,0 +1,31 @@
+//
+//  CountryColor.swift
+//  FeedExplorer
+//
+//  Created by redpanda on 2021-02-13.
+//
+
+import UIKit
+
+extension UIColor {
+    struct CountryColor {
+        static var Canada: UIColor
+        { return UIColor(red: 244, green: 67, blue: 54, alpha: 1) }
+        
+        static var UnitedStates: UIColor
+        { return UIColor(red: 224, green: 64, blue: 251, alpha: 1) }
+        
+        static var France: UIColor
+        { return UIColor(red: 63, green: 81, blue: 181, alpha: 1) }
+
+        static var UnitedKingdom: UIColor
+        { return UIColor(red: 139, green: 195, blue: 74, alpha: 1) }
+
+        static var Germany: UIColor
+        { return UIColor(red: 255, green: 193, blue: 7, alpha: 1) }
+        
+        static var Other: UIColor
+        { return UIColor(red: 0, green: 188, blue: 212, alpha: 1) }
+    }
+}
+
